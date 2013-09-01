55d798ec2c21e196d8c51bcc4a9bc33a0adc685d
Adam <hoganajjunk@gmail.com>
2013-09-01 02:44:38 -0400

    Update the bootstrap.

--- hadoop/bootstrap.sh
+++ hadoop/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-sudo yum -y install python-pip libxml2-devel python-setuptools libxslt-devel gcc gcc-c++ python-devel libxml2 libxslt libxslt-dev
+sudo apt-get -y install python-pip libxml2 libxml2-dev libxslt1.1 libxslt1-dev build-essential python-libxslt1 python-dev
+sudo easy_install -U distribute
 sudo pip install requests nltk lxml
